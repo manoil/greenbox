fe6c833570bc4689713b1b87fd2f883741623951
manoil <34388992+manoil@users.noreply.github.com>
2016-09-30 23:19:46 -0500

    Update 2016-09-30T23:19:46.294648

--- start_greenbox.bat
+++ start_greenbox.bat
@@ -1,3 +1,25 @@
 
 python "C:\Program Files\Python37\Lib\manim\manim.py" greenbox.py
 pause
+
+@echo off
+
+echo                                      Git Auto Upload
+echo ===================================================================================
+echo.
+
+::set /p change=请输入Git更新变动：
+echo.
+cd "D:\Developments\Git\greenbox"
+
+git pull
+git add .
+git commit -m upadtes
+git push
+
+echo.
+echo ===================================================================================
+echo                                      Up to date
+echo.
+
+pause
