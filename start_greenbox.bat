31d0b8ef1f01a0e9e8e4b820aef4e5815a4af58a
manoil <34388992+manoil@users.noreply.github.com>
2019-09-30 17:46:19 -0500

    Update 2019-09-30T17:46:19.346147

--- start_greenbox.bat
+++ start_greenbox.bat
@@ -3,7 +3,7 @@
 title=Green Box Auto Bat
 mode con cols=40 lines=20
 color 02
-                                    
+
 echo ============GitHub Green Box============
 echo.
 echo.
@@ -20,6 +20,5 @@ cls
 python greenbox.py
 
 :end
-echo 
 set /p input=Type "1" to restart the program:
 if "%input%"=="1" goto running
