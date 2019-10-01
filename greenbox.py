e98551e81142bf4ae92c142c26ea5a4d576f382c
manoil <34388992+manoil@users.noreply.github.com>
2019-10-01 10:17:57 -0500

    Update 2019-10-01T10:17:57.967123

--- greenbox.py
+++ greenbox.py
@@ -37,7 +37,7 @@ while commit_date < now:
         else:
             if commit_num < how_many_commit_this_time:
                 commit_num += 1
-                os._exit()
+                os._exit(0)
 
             else:
                 how_many_commit_this_time = 0
