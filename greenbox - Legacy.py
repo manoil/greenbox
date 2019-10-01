ebf567e767e52470b2a20aeddaf73d938c0b82e3
manoil <34388992+manoil@users.noreply.github.com>
2019-10-01 06:01:53 -0500

    Update 2019-10-01T06:01:53.821763

--- greenbox - Legacy.py
+++ greenbox - Legacy.py
@@ -21,26 +21,28 @@ def change_time():
 commit_date = (start + change_day())
 times = randint(60, 300)
 commit_num = 0
-how_many_commit_this_time = randint(min_commit_num + 1, min_commit_num + commit_range)
+how_many_commit_this_time = min_commit_num
 
 while commit_date < now:
 
     commit_date = commit_date + change_day()
     for i in range(times):
-        print("---NO." + str(commit_num + 1) + "commit---")
         f = open('data.txt', 'a+')
         commit_date = commit_date + change_time()
         f.writelines(commit_date.isoformat() + '\nWhat a nice day!\n')
         f.close()
         os.system('git add .')
         os.system('git commit --date={time} -m "Update {time}"'.format(time=commit_date.isoformat()))
-
-        if commit_num < how_many_commit_this_time:
+        if commit_num == min_commit_num:
+            how_many_commit_this_time = randint(min_commit_num + 1, min_commit_num + commit_range)
             commit_num += 1
         else:
-            print('\n============pushing============\n')
-            how_many_commit_this_time = 0
-            os.system('git push')
-            commit_num = 0
-            print("Finished!")
-            sys.exit(0)
+            if commit_num < how_many_commit_this_time:
+                commit_num += 1
+                print("NO." + str(commit_num))
+            else:
+                print('\n============pushing============\n')
+                how_many_commit_this_time = 0
+                os.system('git push')
+                commit_num = 0
+                sys.exit(0)
