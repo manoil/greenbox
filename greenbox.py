7e19733fe02eb6976599b32bb9c23ed8ad9d159c
manoil <34388992+manoil@users.noreply.github.com>
2019-10-01 20:43:47 -0500

    Update 2019-10-01T20:43:47.148871

--- greenbox.py
+++ greenbox.py
@@ -28,8 +28,8 @@ while commit_date < now:
         f.close()
         os.system('git add .')
         os.system('git commit --date={time} -m "Update {time}"'.format(time=commit_date.isoformat()))
-        if commit_num > 10:
-            if commit_num > randint(10, 20):
+        if commit_num > 250:
+            if commit_num > randint(250, 300):
                 os.system('git push')
                 commit_num = 0
         else:
